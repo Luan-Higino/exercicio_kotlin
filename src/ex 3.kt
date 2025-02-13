d700d3189e893868526e61e605bf1a9c6b4c0b28
Luan-Higino <luanmirandah12@gmail.com>
2025-02-13 11:22:35 -0300

    Initial commit

---
+++ src/ex 3.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Nome do funcionário:")
+    var nome = readLine()
+
+    println("Salário:")
+    var salario = readln()
+
+    println("O funcionario $nome tem um salario de R$$salario em Junho.")
+}
---
+++ src/ex 4.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Digite um valor:")
+    var a = readln().toInt()
+    println("Digite outro valor:")
+    var b = readln().toInt()
+
+    println("A soma entre $a e $b é igual a ${a+b}")
+}
