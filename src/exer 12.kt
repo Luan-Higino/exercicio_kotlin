004541cd21996f692c69b20ff51577dc6352e43c
Luan-Higino <luanmirandah12@gmail.com>
2025-02-13 16:42:06 -0300

    .

---
+++ src/exer 12.kt
@@ -0,0 +1,13 @@
+/**
+ * Crie um programa que leia o preço de um produto, calcule e
+ * mostre o seu PREÇO PROMOCIONAL, com 5% de desconto.
+ */
+
+fun main(){
+    println("Preço do produto:")
+    var produto = readln().toDouble()
+    val desconto = 0.05
+    val soma = desconto * produto
+    println("O desconto de $produto, ficou ${produto - soma}")
+
+}
