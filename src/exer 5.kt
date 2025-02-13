1c0d8d1e1f0a91d7efa24a883443695ec278715b
Luan-Higino <luanmirandah12@gmail.com>
2025-02-13 11:39:48 -0300

    subindo arquivo

---
+++ src/exer 5.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Nota 1:")
+    var nota1 = readln().toDouble()
+    println("Nota 2:")
+    var nota2 = readln().toDouble()
+
+    println("A média entre $nota1 e $nota2 é igual a ${(nota1 + nota2)/2}")
+}
---
+++ src/exer 6.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Digite um numero:")
+    var a = readln().toInt()
+    println("O antecessor de $a é ${a-1}")
+    println("O sucessor de $a é ${a+1}")
+}
