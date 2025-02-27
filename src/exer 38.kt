75c849e6273a615c977f547493439f8fd4929014
Luan-Higino <luanmirandah12@gmail.com>
2025-02-27 11:29:31 -0300

    Subindo arquivo

---
+++ src/exer 38.kt
@@ -0,0 +1,10 @@
+///Escreva um programa que mostre na tela a seguinte contagem:
+//
+//6 7 8 9 10 11 Acabou!
+
+fun main() {
+    for (i in 6..11) {
+        print("$i ")
+    }
+    println("Acabou!")
+}
