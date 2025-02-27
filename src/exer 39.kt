518fee40593a251feb78339dfdd85478971be460
Luan-Higino <luanmirandah12@gmail.com>
2025-02-27 11:32:57 -0300

    Subindo arquivo

---
+++ src/exer 39.kt
@@ -0,0 +1,10 @@
+///Faça um algoritmo que mostre na tela a seguinte contagem:
+//
+//10 9 8 7 6 5 4 3 Acabou!
+
+fun main(){
+    for (i in 10 downTo 3){
+        print("$i ")
+    }
+    print("Acabou!")
+}
