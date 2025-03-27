231401747e2c00da366324e7ec723fdf21db2269
Luan Miranda Higino <luanmirandah12@gmail.com>
2025-03-27 16:27:39 -0300

    Procedimento para Exibir Mensagem Formatada

---
+++ src/exer 87.kt
@@ -0,0 +1,17 @@
+///Crie um programa que tenha um procedimento Gerador() que,
+//quando chamado, mostre a mensagem &quot;Olá, Mundo!&quot; com algum
+//componente visual (linhas)
+//Ex: Ao chamar Gerador() aparece:
+//+ ======= +
+//Olá, Mundo!
+//+ ======= +
+
+fun gerador(){
+    println("+ ======= +")
+    println("Olá, Mundo!")
+    println("+ ======= +")
+}
+
+fun main(){
+    gerador()
+}
